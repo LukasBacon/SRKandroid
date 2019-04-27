815ea10d0c528f57f64367afb37e76ca0c4c8267
LukasBacon <lukasslaninka19@gmail.com>
2019-04-27 17:06:27 +0200

    set production api url

--- app/src/main/java/com/lukas/srkandroid/API.java
+++ app/src/main/java/com/lukas/srkandroid/API.java
@@ -5,7 +5,7 @@ public enum API {
     PROD_URL("http://rybarskyklub.sk/rest"),
     TEST_URL("http://192.168.0.106:8080/SRK/rest");
 
-    public static String URL = TEST_URL.toString();
+    public static String URL = PROD_URL.toString();
 
     private String url;
 
